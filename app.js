41b1e68331f4b7043879be0ada342b5e9d2883db
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-07-06 14:33:23 +0300

    refactor: поменять порт

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(errorHandler);
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb')
   .then(() => {
-    app.listen(3000);
+    app.listen(3111);
   })
   .catch(() => {
     process.exit();
