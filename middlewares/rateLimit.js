1be0f2f7e3c45e17c8a8df785a942059ecf9e70c
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-08-26 13:50:51 +0300

    fix: отслеживать ошибку 409

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,8 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 25 * 60 * 1000, // не более 200 запросов за 25 минут
-  max: 200,
+  windowMs: 25 * 60 * 1000, // за 25 минут
+  max: 200, // не более 200 запросов
+  message: 'Слишком много запросов, пожалуйста, повторите попытку позже',
 });
 
 module.exports = { limiter };
